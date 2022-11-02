ca4939dc09149b81913c4913c2e58f01a811e7b6
Akash Nayar <akashknayar5@gmail.com>
2022-11-02 19:55:28 -0400

    remove test.ml (we need another way to test

--- src/test.ml
+++
@@ -1,6 +0,0 @@
-(* open Ast
-
-let _ =
-  let lexbuf = Lexing.from_channel (open_in "./test_src/input.bruh") in
-  let prog = Parser.program Scanner.token lexbuf in
-  let _ = do_program prog in 0 *)
