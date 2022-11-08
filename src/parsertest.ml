e9f2c4c9a2567039700b28906c17900d84ee5a00
Joshua Zhou <joshuazhou35@gmail.com>
2022-11-08 13:09:17 -0500

    Make makefile more flexible

--- src/parsertest.ml
+++ src/parsertest.ml
@@ -11,8 +11,6 @@ let deflate token =
         | hd::t -> List.iter (fun tok -> Queue.add tok q) t ; hd 
 
 let _ =
-  let lexbuf = Lexing.from_channel (open_in "./test_src/parserinput.bruh") in
+  let lexbuf = Lexing.from_channel stdin in
   let program = Parser.program (deflate Scanner.token) lexbuf in
-  let output = open_out "./test_src/parseroutput.txt" in
-  Printf.fprintf output "%s\n" (string_of_program program);
-  close_out output;
+  Printf.fprintf stdout "%s\n" (string_of_program program);
