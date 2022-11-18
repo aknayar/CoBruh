772370cbe0e27bcdaea948f3c154f40c576e1bbd
Joshua Zhou <joshuazhou35@gmail.com>
2022-11-18 14:55:17 -0500

    Update sast

--- src/sast.ml
+++ src/sast.ml
@@ -4,17 +4,29 @@ type sexpr = dtype * sx
 and sx = 
     SNumberLit of float
   | SBoolLit of bool
-  | SStringLit of string
   | SCharLit of char
+  | SStringLit of string
   | SId of string
   | SBinop of sexpr * bop * sexpr
   | SUnop of uop * sexpr
   | SCall of string * sexpr list
+  | SElem of string * sexpr
+
+type sarray_element =
+    SExprElem of sexpr
+  | SArrayElem of sarray_element list
+
+type sarray_assign = 
+    SArrayLit of sarray_element list
+  | SArrayId of string
 
 type sstmt = 
     SExpr of sexpr
   | SAssign of dtype * string * sexpr
-  | SReassign of string * sexpr
+  | SInferAssign of string * sexpr
+  | SAlloc of dtype * string * sexpr list
+  | SAllocAssign of dtype * string * sexpr list * sarray_assign
+  | SAllocInferAssign of string * sexpr list * sarray_assign
   | SIf of sexpr * sstmt list * sstmt list
   | SIterLoop of string * sexpr * sexpr * sexpr * sstmt list
   | SCondLoop of sexpr * sstmt list
