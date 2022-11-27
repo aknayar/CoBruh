78c8d304b47342c14fbb61ebbe7ce226b05ae80e
Joshua Zhou <joshuazhou35@gmail.com>
2022-11-27 15:39:35 -0500

    Tests

--- test/parsertest.ml
+++ test/parsertest.ml
@@ -1,16 +1,15 @@
-open Ast
-open Parser
+open CoBruh
 
 let deflate token = 
   let q = Queue.create () in
   fun lexbuf -> 
     if not (Queue.is_empty q) then Queue.pop q else   
       match token lexbuf with 
-        | [] -> EOF 
+        | [] -> Parser.EOF 
         | [tok] -> tok
-        | hd::tl -> List.iter (fun tok -> Queue.add tok q) tl ; hd 
+        | hd::tl -> List.iter (fun tok -> Queue.add tok q) tl; hd 
 
 let _ =
   let lexbuf = Lexing.from_channel stdin in
   let program = Parser.program (deflate Scanner.token) lexbuf in
-  Printf.fprintf stdout "%s\n" (string_of_program program);
+  Printf.fprintf stdout "%s\n" (Ast.string_of_program program);
--- test/semanttest.ml
+++ test/semanttest.ml
@@ -1,14 +1,13 @@
-open Sast
-open Parser
+open CoBruh
 
 let deflate token = 
   let q = Queue.create () in
   fun lexbuf -> 
     if not (Queue.is_empty q) then Queue.pop q else   
       match token lexbuf with 
-        | [] -> EOF 
+        | [] -> Parser.EOF 
         | [tok] -> tok
-        | hd::tl -> List.iter (fun tok -> Queue.add tok q) tl ; hd 
+        | hd::tl -> List.iter (fun tok -> Queue.add tok q) tl; hd 
 
 let _ =
   let lexbuf = Lexing.from_channel stdin in
