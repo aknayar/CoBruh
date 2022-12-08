da4a02d0efbc06c62c2901d876ab26a2d844505a
Joshua Zhou <joshuazhou35@gmail.com>
2022-12-08 18:29:33 -0500

    Builds :)

--- lib/irgen.ml
+++ lib/irgen.ml
@@ -18,7 +18,7 @@ let translate (binds, sfuncs): L.llmodule =
     | _ -> raise (Failure "unimplemented") 
   in
 
-  let globals = 
+  let _ = 
     let add_global m (typ, name) = 
       let init = 
         match typ with
@@ -43,44 +43,59 @@ let translate (binds, sfuncs): L.llmodule =
   in
 
   let build_func_body fn = 
+    let rec build_expr builder (_, exp) = match exp with
+        SNumberLit n -> L.const_float f_t n
+      | SBoolLit b -> L.const_int i1_t (if b then 1 else 0)
+      | SBinop (e1, op, e2) ->
+          let e1' = build_expr builder e1
+          and e2' = build_expr builder e2 in
+          (
+            match op with
+                Plus    -> L.build_fadd
+              | Minus   -> L.build_fsub
+              | Times   -> L.build_fmul
+              | Div     -> L.build_fdiv
+              | And     -> L.build_and
+              | Or      -> L.build_or
+              | Eq      -> L.build_icmp L.Icmp.Eq
+              | Neq     -> L.build_icmp L.Icmp.Ne
+              | Less    -> L.build_icmp L.Icmp.Slt
+              | _         -> raise (Failure "unimplemented")
+          ) e1' e2' "tmp" builder
+      | SCall ("say", [e]) ->
+          let num_format_str = L.build_global_stringptr "%g\n" "fmt" builder in
+          L.build_call printf_func [| num_format_str ; (build_expr builder e) |] "printf" builder
+      | _ -> raise (Failure "unimplemented")
+    in
+
+    let add_terminal builder instr = 
+      match L.block_terminator (L.insertion_block builder) with
+          Some _ -> ()
+        | None -> ignore (instr builder)
+    in
+
+    let build_stmt sc builder = function
+        SExpr sexp -> ignore (build_expr builder sexp); builder
+      | SInit (id, sexp) -> 
+          let local = L.build_alloca (lltype_of_dtype (fst sexp)) id builder
+          in Hashtbl.add sc id local;
+          let sexp' = build_expr builder sexp in
+          ignore (L.build_store sexp' local builder); builder
+      | _ -> raise (Failure "unimplemented")
+    in
+    
+    let func_scope = ref [Hashtbl.create 10] in
     let (the_func, _) = StringMap.find fn.sfname all_funcs in
     let builder = L.builder_at_end context (L.entry_block the_func) in
-    
+    let builder = List.fold_left (
+      fun b s -> build_stmt (List.hd !func_scope) b s
+    ) builder fn.sbody in
 
-  let rec build_expr builder (_, exp) = match exp with
-      SNumberLit n -> L.const_float f_t n
-    | SBoolLit b -> L.const_int i1_t (if b then 1 else 0)
-    | SBinop (e1, op, e2) ->
-      let e1' = build_expr builder e1
-      and e2' = build_expr builder e2 in
-      (
-        match op with
-            Plus    -> L.build_fadd
-          | Minus   -> L.build_fsub
-          | Times   -> L.build_fmul
-          | Div     -> L.build_fdiv
-          | And     -> L.build_and
-          | Or      -> L.build_or
-          | Eq      -> L.build_icmp L.Icmp.Eq
-          | Neq     -> L.build_icmp L.Icmp.Ne
-          | Less    -> L.build_icmp L.Icmp.Slt
-          | _         -> raise (Failure "unimplemented")
-      ) e1' e2' "tmp" builder
-    | SCall ("say", [e]) ->
-      let num_format_str = L.build_global_stringptr "%g\n" "fmt" builder in
-      L.build_call printf_func [| num_format_str ; (build_expr builder e) |]
-        "printf" builder
-    | _ -> raise (Failure "unimplemented")
-  in
+    add_terminal builder (
+      match fn.srtype with
+          None -> L.build_ret_void
+        | typ -> L.build_ret (L.const_float (lltype_of_dtype typ) 0.0)
+    )
 
-  let build_stmt builder = function
-      SExpr sexp -> ignore (build_expr builder sexp); builder
-    | SInit (id, sexp) -> raise (Failure "unimplemented")
-    | _ -> raise (Failure "unimplemented") in
-
-  let build_decl builder = function
-      SStmt sstmt -> ignore(build_stmt builder sstmt)
-    | SFunc sfunc -> raise (Failure "unimplemented")
-  in
-  List.iter build_decl prog;
+  in List.iter build_func_body sfuncs;
   mdl
