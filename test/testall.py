3fc6587cde9adb1cffae3e7893de78f7575f3af2
Lance Wong <ldwong20@gmail.com>
2022-12-14 18:15:50 -0500

    inital testall.py file with 3 basic test cases

---
+++ test/testall.py
@@ -0,0 +1,48 @@
+import os
+import subprocess
+
+# all the test files are in /tests
+directory = os.fsencode("./tests")
+    
+for file in os.listdir(directory):
+    # run the file
+    # get the output
+    # compare with corresponding .err or .out file
+    # if it doens't match, throw an error describing what went wrong
+    filename = os.fsdecode(file)
+    if filename.endswith(".err"): 
+        file_ans_loc = os.path.join("./tests/", filename)
+        file_loc = os.path.join("./tests/", '.'.join(filename.split('.')[:-1] + ["bruh"]))
+  
+        result = subprocess.run(f"dune exec -- CoBruh -c {file_loc}".split(), stdout=subprocess.PIPE)
+        result = result.stdout.decode("utf-8") 
+
+        ans = ''
+        with open(file_ans_loc, 'r') as f:
+            ans = f.read()
+        
+        if not ans.strip() == result.strip():
+            raise Exception("Wrong TC, please check code")
+    if filename.endswith(".out"):
+        file_ans_loc = os.path.join("./tests/", filename)
+        file_loc = os.path.join("./tests/", '.'.join(filename.split('.')[:-1] + ["bruh"]))
+  
+        ir_result = subprocess.run(f"dune exec -- CoBruh -c {file_loc}".split(), stdout=subprocess.PIPE)
+        with open("main.ll", 'w') as writer:
+            writer.write(ir_result.stdout.decode("utf-8"))
+        result = subprocess.run(f"lli main.ll".split(), stdout=subprocess.PIPE)
+        result = result.stdout.decode("utf-8") 
+
+        ans = ''
+        with open(file_ans_loc, 'r') as f:
+            ans = f.read()
+      
+        if not ans.strip() == result.strip():
+            raise Exception("Wrong TC, please check code")
+     
+
+
+
+
+
+
